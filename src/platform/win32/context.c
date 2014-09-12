68514f72f892a55d4857bf60eb7dc2deb6dc79ee
Lukas Hermanns <lhlukash23@googlemail.com>
2014-09-12 21:41:39 +0200

    - Further directory refactoring.

---
+++ src/platform/win32/context.c
@@ -0,0 +1,10 @@
+/*
+ * context.c (WIN32)
+ * 
+ * This file is part of the "PicoRenderer" (Copyright (c) 2014 by Lukas Hermanns)
+ * See "LICENSE.txt" for license information.
+ */
+
+#include "context.h"
+
+
--- src/platform/win32/context.h
+++ src/platform/win32/context.h
@@ -1,16 +1,15 @@
 /*
- * context.h
+ * context.h (WIN32)
  * 
  * This file is part of the "PicoRenderer" (Copyright (c) 2014 by Lukas Hermanns)
  * See "LICENSE.txt" for license information.
  */
 
-#ifndef __PR_ERROR_H__
-#define __PR_ERROR_H__
+#ifndef __PR_CONTEXT_H__
+#define __PR_CONTEXT_H__
 
 
 #include "types.h"
-#include "error_ids.h"
 
 
 void _pr_error_set(PRenum errorID);
